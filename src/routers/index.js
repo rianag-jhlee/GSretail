4eccec14b5d0f70ba9afaf91ebc94a83cc10353b
jhlee <jhlee@rianag.com>
2026-03-03 12:35:33 +0900

    퍼블 화면 /uiPub

--- src/routers/index.js
+++ src/routers/index.js
@@ -3,7 +3,7 @@ import { createWebHistory, createRouter } from "vue-router";
 const routes = [
     {
         path: "/",
-        component: () => import("@/views/gsr00/gsr-00-00-00")
+        component: () => import("@/uiPub/gsr00/gsr-00-00-00")
     }
 ];
 
