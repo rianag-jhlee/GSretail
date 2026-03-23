a92d36e83fb371f2d5704ef7ea9cc1db7fd7ad19
jhlee <jhlee@rianag.com>
2026-03-23 15:56:23 +0900

    input css 수정

--- src/guide/form.vue
+++ src/guide/form.vue
@@ -6,7 +6,8 @@
         <section>
             <h4>Inputs</h4>
             <ul class="explain">
-                <li><strong>type</strong> : text/password/check/radio/switch input type 선택 // default="text"</li>
+                <li><strong>type</strong> : text/password/check/radio/switch 선택 // default="text"</li>
+                <li><strong>isswitch</strong> : true/false ※checkbox의 스위치 형태 // default="false"</li>
             </ul>
 
             <p class="tit_wrap">
@@ -99,43 +100,43 @@ export default {
             switch (type) {
                 case "text":
                     this.input_opt.type = "text";
-                    this.input_opt.opt = `:type="${this.input_opt.type}" :placeholder="${this.input_opt.Placeholder}"`;
+                    this.input_opt.opt = `type="${this.input_opt.type}" placeholder="${this.input_opt.Placeholder}"`;
                     break;
 
                 case "text_err":
                     this.input_opt.type = "text";
-                    this.input_opt.class = "err";
-                    this.input_opt.opt = `:type="${this.input_opt.type}" class="err"`;
+                    this.input_opt.isError = true;
+                    this.input_opt.opt = `type="${this.input_opt.type}" placeholder="${this.input_opt.Placeholder}" isError="true"`;
                     break;
 
                 case "error":
                     this.input_opt.type = "text";
-                    this.input_opt.opt = `:type="${this.input_opt.type}" :placeholder="${this.input_opt.Placeholder}"`;
                     this.input_opt.isswitch = false;
                     this.input_opt.isError = true;
-                    this.input_opt.err_text = "Error Text";
+                    this.input_opt.err_text = "에러 노출 문구";
+                    this.input_opt.opt = `type="${this.input_opt.type}" placeholder="${this.input_opt.Placeholder}" isError="${this.input_opt.isError}" errText="${this.input_opt.err_text}"`;
                     break;
 
                 case "pw":
                     this.input_opt.type = "password";
                     this.input_opt.Placeholder = "비밀번호를 입력해주세요";
-                    this.input_opt.opt = `:type="${this.input_opt.type}" :placeholder="${this.input_opt.Placeholder}"`;
+                    this.input_opt.opt = `type="${this.input_opt.type}" placeholder="${this.input_opt.Placeholder}"`;
                     break;
 
                 case "check":
                     this.input_opt.type = "checkbox";
-                    this.input_opt.opt = `:type="${this.input_opt.type}" text="Label"`;
+                    this.input_opt.opt = `type="${this.input_opt.type}" text="Label"`;
                     break;
 
                 case "radio":
                     this.input_opt.type = "radio";
-                    this.input_opt.opt = `:type="${this.input_opt.type}" text="Label"`;
+                    this.input_opt.opt = `type="${this.input_opt.type}" text="Label"`;
                     break;
 
                 case "isswitch":
                     this.input_opt.type = "checkbox";
                     this.input_opt.isswitch = true;
-                    this.input_opt.opt = `_type="${this.input_opt.type}" :isswitch="${this.input_opt.isswitch}" text="Label"`;
+                    this.input_opt.opt = `type="${this.input_opt.type}" isswitch="${this.input_opt.isswitch}" text="Label"`;
                     break;
             }
         },
--- src/guide/guide_wrap.vue
+++ src/guide/guide_wrap.vue
@@ -45,39 +45,32 @@ export default {
 }
 </script>
 
-<style>
+<style scope>
 @import "@/assets/css/ui.css";
 @import "@/assets/css/common.css";
 
 #guide_container {margin:50px auto; padding:0 20px;}
-
 #guide_wrap h2 {margin-bottom:20px; padding-bottom:5px; color:#009999; font-size:2.4rem; border-bottom:1px dotted #d4d4d4;}
-
-#guide_wrap section+section {margin-top:20px; padding-top:20px; border-top:1px dashed #d4d4d4;}
+#guide_wrap section + section {margin-top:20px; padding-top:20px; border-top:1px dashed #d4d4d4;}
 
 #guide_wrap .design_box {margin-top:10px; padding:10px; border:1px solid #ddd;}
 
 #guide_wrap .tit_wrap a {margin-left:5px; font-size:0; font-weight:500; vertical-align:middle; display:inline-block;}
-
 #guide_wrap .tit_wrap a:before {padding-right:5px; color:#333; font-size:1.2rem; vertical-align:top; content:':'; display:inline-block;}
-
 #guide_wrap .tit_wrap a:after {color:#333; font-size:1rem; content:'OPEN'; vertical-align:top; display:inline-block;}
-
 #guide_wrap .tit_wrap a.active:after {content:'CLOSE';}
-
 #guide_wrap strong.tit {margin-top:10px; padding:3px 10px; color:#fff; font-size:1.1rem; font-weight:500; line-height:1; background-color:#009999; border-radius:100px; display:inline-block;}
 
 #guide_wrap .code_box {margin-top:10px; padding:20px; color:#fff; font-size:14px; font-family:dotum !important; line-height:1.4; white-space:pre-wrap; background-color:#000; border:1px solid #ddd; display:none;}
 #guide_wrap .code_box code {color:#fff; font-size:1.2rem;}
 
 #guide_wrap .explain {margin-top:10px; margin-bottom:10px;}
-
 #guide_wrap .explain li {padding-left:10px; font-size:1.2rem; position:relative;}
 #guide_wrap .explain li strong {font-size:1.2rem;}
-
 #guide_wrap .explain li + li {margin-top:5px;}
-
 #guide_wrap .explain li:before {width:3px; height:3px; background-color:#666; content:''; position:absolute; top:10px; left:0; display:inline-block;}
-
 #guide_wrap .explain li em {font-size:1.2rem;}
+
+#guide_wrap .toolbar {margin-top:12px; display:flex; gap:4px;}
+#guide_wrap .toolbar button {padding:4px 8px; color:#fff; background-color:#222; border-radius:3px; cursor:pointer;}
 </style>
