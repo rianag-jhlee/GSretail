0a22cdd17bccafb235d01e23ba7eab64bf583ad5
이소라 <srlee@rianag.com>
2026-05-22 10:54:02 +0900

    팝업 스크롤 오류 수정

--- src/uiPub/popUp/gsrst010201.vue
+++ src/uiPub/popUp/gsrst010201.vue
@@ -1,106 +1,108 @@
 <template>
-    <div class="smn_modal">
-        <div class="smn_title_row">
-            <h2 class="smn_title">사업설명회 신청</h2>
-            <button type="button" class="smn_close" aria-label="닫기" @click="closeModal"></button>
-        </div>
-
-        <div class="modal_content smn_body">
-            <div class="smn_intro">
-                <p>사업설명회 신청을 하시려면 아래 내용을 입력해 주시기 바랍니다.</p>
+    <div class="modal_cont">
+        <div class="smn_modal">
+            <div class="smn_title_row">
+                <h2 class="smn_title">사업설명회 신청</h2>
+                <button type="button" class="smn_close" aria-label="닫기" @click="closeModal"></button>
             </div>
 
-            <div class="consent_box">
-                <div class="consent_info">
-                    <h3 class="consent_info_title">개인정보 수집·이용 동의</h3>
-                    <ul class="consent_info_list">
-                        <li>입력하신 정보는 사업설명회에 의한 확인을 위해서만 사용합니다. 수집항목, 이용 및 목적, 보유 및 이용기간은 다음과 같습니다.<br>기타 개인정보 취급사항은 홈페이지 하단의 "개인정보 취급방침"을 참고하시기 바랍니다.</li>
-                        <li>수집하는 개인정보 항목 : 이름, 연락처, E-mail, 지역</li>
-                        <li>수집이용 및 목적: 수집된 개인정보를 통한 사업 및 문의사항 확인 및 답변을 위하여 활용</li>
-                        <li>보유 및 이용기간: 개인정보 수집 및 이용 목적이 달성된 후에는 예외없이 정보를 파기 합니다.</li>
-                    </ul>
-                    <div class="consent_notice">
-                        <p class="consent_notice_text">고객님께서는 본 동의에 거부하실 권리가 있으나, 동의하지 않으실 경우<br class="p_br">사업설명회 신청 글 작성이 불가능합니다.</p>
-                        <label class="consent_radio_label">
-                            <input type="radio" class="consent_radio" name="consent_agree" value="agree" v-model="consentAgree" @click="consentAgree = consentAgree === 'agree' ? '' : 'agree'" />
-                            <span class="consent_radio_text">동의합니다.</span>
-                        </label>
+            <div class="modal_content smn_body">
+                <div class="smn_intro">
+                    <p>사업설명회 신청을 하시려면 아래 내용을 입력해 주시기 바랍니다.</p>
+                </div>
+
+                <div class="consent_box">
+                    <div class="consent_info">
+                        <h3 class="consent_info_title">개인정보 수집·이용 동의</h3>
+                        <ul class="consent_info_list">
+                            <li>입력하신 정보는 사업설명회에 의한 확인을 위해서만 사용합니다. 수집항목, 이용 및 목적, 보유 및 이용기간은 다음과 같습니다.<br>기타 개인정보 취급사항은 홈페이지 하단의 "개인정보 취급방침"을 참고하시기 바랍니다.</li>
+                            <li>수집하는 개인정보 항목 : 이름, 연락처, E-mail, 지역</li>
+                            <li>수집이용 및 목적: 수집된 개인정보를 통한 사업 및 문의사항 확인 및 답변을 위하여 활용</li>
+                            <li>보유 및 이용기간: 개인정보 수집 및 이용 목적이 달성된 후에는 예외없이 정보를 파기 합니다.</li>
+                        </ul>
+                        <div class="consent_notice">
+                            <p class="consent_notice_text">고객님께서는 본 동의에 거부하실 권리가 있으나, 동의하지 않으실 경우<br class="p_br">사업설명회 신청 글 작성이 불가능합니다.</p>
+                            <label class="consent_radio_label">
+                                <input type="radio" class="consent_radio" name="consent_agree" value="agree" v-model="consentAgree" @click="consentAgree = consentAgree === 'agree' ? '' : 'agree'" />
+                                <span class="consent_radio_text">동의합니다.</span>
+                            </label>
+                        </div>
+                    </div>
+                </div>
+
+                <div class="apply_form">
+                    <div class="form_head">
+                        <h3 class="form_head_title">신청하기</h3>
+                        <span class="form_required_note">* 필수 입력사항</span>
+                    </div>
+                    <div class="form_body">
+                        <div class="form_row">
+                            <div class="form_label">이름<span class="form_required">*</span></div>
+                            <div class="form_field">
+                                <Inputs type="text" v-model="seminarForm.name" />
+                            </div>
+                        </div>
+                        <div class="form_row">
+                            <div class="form_label">연락처<span class="form_required">*</span></div>
+                            <div class="form_field form_field_phone">
+                                <SelectBox :options="phoneOptions" v-model="seminarForm.phone1" />
+                                <span class="form_sep">-</span>
+                                <Inputs type="text" v-model="seminarForm.phone2" />
+                                <span class="form_sep">-</span>
+                                <Inputs type="text" v-model="seminarForm.phone3" />
+                            </div>
+                        </div>
+                        <div class="form_row">
+                            <div class="form_label">이메일<span class="form_required">*</span></div>
+                            <div class="form_field form_field_email">
+                                <Inputs type="text" v-model="seminarForm.emailId" />
+                                <span class="form_sep">@</span>
+                                <Inputs v-if="seminarForm.emailDomain === ''" type="text" v-model="seminarForm.emailDomainCustom" placeholder="직접입력" />
+                                <Inputs v-else type="text" :model-value="seminarForm.emailDomain" :is-readonly="true" />
+                                <SelectBox :options="emailDomainOptions" v-model="seminarForm.emailDomain" initMsg="직접입력" />
+                            </div>
+                        </div>
+                        <div class="form_row">
+                            <div class="form_label">개설희망지역<span class="form_required">*</span></div>
+                            <div class="form_field form_field_region">
+                                <SelectBox :options="regionSidoOptions" v-model="seminarForm.regionSido" initMsg="지역선택" />
+                                <SelectBox :options="[]" v-model="seminarForm.regionSigungu" initMsg="구/군 선택" />
+                            </div>
+                        </div>
+                        <div class="form_row">
+                            <div class="form_label">재직중인 과거직장</div>
+                            <div class="form_field">
+                                <Inputs type="text" v-model="seminarForm.prevJob" />
+                            </div>
+                        </div>
+                        <div class="form_row form_row_radio">
+                            <div class="form_label">사업 설명회 신청<br class="p_br">지역<span class="form_required">*</span></div>
+                            <div class="form_field form_field_radio">
+                                <Inputs
+                                    v-for="opt in seminarRegionOptions"
+                                    :key="opt.value"
+                                    type="radio"
+                                    name="seminar_region"
+                                    :value="opt.value"
+                                    v-model="seminarForm.seminarRegion"
+                                    :text="opt.label"
+                                />
+                            </div>
+                        </div>
+                        <div class="form_row form_row_textarea">
+                            <div class="form_label">내용<span class="form_required">*</span></div>
+                            <div class="form_field">
+                                <Textarea v-model="seminarForm.content" :placeholder="contentPlaceholder" :rows="6" />
+                            </div>
+                        </div>
                     </div>
                 </div>
             </div>
 
-            <div class="apply_form">
-                <div class="form_head">
-                    <h3 class="form_head_title">신청하기</h3>
-                    <span class="form_required_note">* 필수 입력사항</span>
-                </div>
-                <div class="form_body">
-                    <div class="form_row">
-                        <div class="form_label">이름<span class="form_required">*</span></div>
-                        <div class="form_field">
-                            <Inputs type="text" v-model="seminarForm.name" />
-                        </div>
-                    </div>
-                    <div class="form_row">
-                        <div class="form_label">연락처<span class="form_required">*</span></div>
-                        <div class="form_field form_field_phone">
-                            <SelectBox :options="phoneOptions" v-model="seminarForm.phone1" />
-                            <span class="form_sep">-</span>
-                            <Inputs type="text" v-model="seminarForm.phone2" />
-                            <span class="form_sep">-</span>
-                            <Inputs type="text" v-model="seminarForm.phone3" />
-                        </div>
-                    </div>
-                    <div class="form_row">
-                        <div class="form_label">이메일<span class="form_required">*</span></div>
-                        <div class="form_field form_field_email">
-                            <Inputs type="text" v-model="seminarForm.emailId" />
-                            <span class="form_sep">@</span>
-                            <Inputs v-if="seminarForm.emailDomain === ''" type="text" v-model="seminarForm.emailDomainCustom" placeholder="직접입력" />
-                            <Inputs v-else type="text" :model-value="seminarForm.emailDomain" :is-readonly="true" />
-                            <SelectBox :options="emailDomainOptions" v-model="seminarForm.emailDomain" initMsg="직접입력" />
-                        </div>
-                    </div>
-                    <div class="form_row">
-                        <div class="form_label">개설희망지역<span class="form_required">*</span></div>
-                        <div class="form_field form_field_region">
-                            <SelectBox :options="regionSidoOptions" v-model="seminarForm.regionSido" initMsg="지역선택" />
-                            <SelectBox :options="[]" v-model="seminarForm.regionSigungu" initMsg="구/군 선택" />
-                        </div>
-                    </div>
-                    <div class="form_row">
-                        <div class="form_label">재직중인 과거직장</div>
-                        <div class="form_field">
-                            <Inputs type="text" v-model="seminarForm.prevJob" />
-                        </div>
-                    </div>
-                    <div class="form_row form_row_radio">
-                        <div class="form_label">사업 설명회 신청<br class="p_br">지역<span class="form_required">*</span></div>
-                        <div class="form_field form_field_radio">
-                            <Inputs
-                                v-for="opt in seminarRegionOptions"
-                                :key="opt.value"
-                                type="radio"
-                                name="seminar_region"
-                                :value="opt.value"
-                                v-model="seminarForm.seminarRegion"
-                                :text="opt.label"
-                            />
-                        </div>
-                    </div>
-                    <div class="form_row form_row_textarea">
-                        <div class="form_label">내용<span class="form_required">*</span></div>
-                        <div class="form_field">
-                            <Textarea v-model="seminarForm.content" :placeholder="contentPlaceholder" :rows="6" />
-                        </div>
-                    </div>
-                </div>
+            <div class="smn_bottom">
+                <Buttons btn-class="btn_big fill primary">신청</Buttons>
+                <Buttons btn-class="btn_big gary" @click="closeModal">취소</Buttons>
             </div>
-        </div>
-
-        <div class="smn_bottom">
-            <Buttons btn-class="btn_big fill primary">신청</Buttons>
-            <Buttons btn-class="btn_big gary" @click="closeModal">취소</Buttons>
         </div>
     </div>
 </template>
@@ -179,10 +181,10 @@ function closeModal(event) {
 </script>
 
 <style scoped>
-.smn_modal { --color-brand-primary: #15b874; }
+.smn_modal { --color-brand-primary: #15b874; height: 100%; min-height: 0; flex: 1; display: flex; flex-direction: column; }
 .smn_modal :deep([class*="btn_"][class*="primary"]) { color: #fff; background-color: var(--color-brand-primary); }
-.modal_content { border-bottom: 1px solid #E5E5E9; }
-.smn_title_row { display: flex; align-items: flex-start; justify-content: space-between; min-height: 92px; margin: 0; padding: 0; }
+.modal_content { min-height: 0; flex: 1; overflow-y: auto; border-bottom: 1px solid #E5E5E9; }
+.smn_title_row { flex-shrink: 0; display: flex; align-items: flex-start; justify-content: space-between; min-height: 92px; margin: 0; padding: 0; }
 .smn_title { color: #161616; font-size: 4rem; font-weight: 700; letter-spacing: -0.01em; line-height: 1.3; }
 .smn_close { width: 40px; height: 40px; background: none; border: none; border-radius: 50%; cursor: pointer; flex-shrink: 0; position: relative; }
 .smn_close::before, .smn_close::after { width: 22px; border-top: 2px solid #161616; content: ""; position: absolute; top: 50%; left: 50%; }
@@ -235,6 +237,7 @@ function closeModal(event) {
 .smn_bottom > :deep(.btn_big) { width: 134px; text-align: center; font-size: 1.6rem; line-height: 1.5; letter-spacing: -0.01em; }
 
 @media (max-width: 768px) {
+    .smn_modal { max-height: 100%; }
     .modal_wrap .modal_container { padding-bottom: 32px; }
     .smn_title_row { min-height: 60px; margin: 0 -20px; padding: 17px 20px; border-bottom: 1px solid #c4c4d0; }
     .smn_title { font-size: 1.8rem; line-height: 1.4; }
