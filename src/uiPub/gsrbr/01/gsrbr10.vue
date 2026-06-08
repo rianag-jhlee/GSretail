7730711785f20d025c289fad485a061c5e5af18d
정다희 <ekgml5233@gmail.com>
2026-06-08 14:16:23 +0900

    feat: bi_box, 이미지 추가 및 교체체

--- src/uiPub/gsrbr/01/gsrbr10.vue
+++ src/uiPub/gsrbr/01/gsrbr10.vue
@@ -26,6 +26,13 @@
                             <a :href="t.HopageLink" class="btn_website" target="_blank">{{ t.HopageText }}</a>
                         </li>
                     </ul>
+                    <!-- 26.06.08 add 정다희 : bi_box 추가-->
+                    <div class="bi_box">
+                        <div class="bi_section">
+                            <h4>{{ t.Bi.txt }}</h4>
+                            <span><img :src="isMobile ? t.Bi.imgMo : t.Bi.img" :alt="t.Bi.alt" /></span>
+                        </div>
+                    </div>
                 </div>
             </div>
             <div class="cont_inner">
@@ -64,7 +71,14 @@ export default {
                     SubContent_2: `별도의 장비 없이 성인이라면 누구나 손쉽게 배달 가능한 서비스입니다.`,
                     HopageLink: `http://woodel.co.kr/#page1`,
                     HopageText: `홈페이지`,
-                    ListBack:`목록으로 돌아가기`
+                    ListBack:`목록으로 돌아가기`,
+                    // 26.06.08 add 정다희 : bi_box 추가-->
+                    Bi: {
+                        txt: `BI 로고`,
+                        img: require("@/assets/images/dummy/gsrbr10_bi.png"),
+                        imgMo: require("@/assets/images/dummy/gsrbr10_bi_mo.png"),
+                        alt: "Real price BI 이미지"
+                    },
                 },
                 en: {
                     Visual: { // 대문자 유지
@@ -80,7 +94,10 @@ export default {
                     SubContent_2: `It is a service that any adult can easily deliver with no special equipment.`/* 260604 번역 */,
                     HopageLink: `http://woodel.co.kr/#page1`,
                     HopageText: `Homepage`,
-                    ListBack:`Back to Brand List`
+                    ListBack:`Back to Brand List`,
+                    Bi: {
+                     
+                    },
                 }
             }
         };
@@ -115,30 +132,42 @@ export default {
 
 /* Visual Section */
 .visual_section { width:100%; position: relative; overflow: hidden; }
-.visual_img { width: 100%; height: auto; }
+/* visual_img css수정 */
+.visual_img { width: 100%; height: auto; position: relative;}
+.visual_img::after{content:"";width: 100%;height: 100%;background:rgba(0,0,0,0.3);position: absolute;top: 0;left: 0;z-index: 1;}
 .visual_img img { width: 100%; display: block; }
 .visual_content { position: absolute; top: 50%; left: 0; transform: translateY(-50%); width: 100%; z-index: 2; }
-.text_box span { color:#fff; font-size:72px; font-weight:700; display: block;  }
+.text_box span { color:#fff; font-size:72px; font-weight:700; display: block; position: relative;z-index: 2; }
 
 /* body_wrap Section */
 .body_wrap .cont_area {padding:10.41%; background:#F8F8F8;}
 .body_wrap .cont_area .cont_inner h3 {margin-bottom:16px; color:#161616; font-size:48px; font-weight:700;}
-.body_wrap .cont_area .cont_inner ul {display:flex; justify-content:space-between; align-items:flex-end;}
+.body_wrap .cont_area .cont_inner ul {margin-bottom:64px; display:flex; justify-content:space-between; align-items:flex-end;}
 .body_wrap .cont_area .cont_inner ul li p {color:#161616; font-size:24px; font-weight:600; line-height:1.;}
 .body_wrap .cont_area .cont_inner ul li a {color:#161616; font-size:18px; display:flex; align-items:center;}
 
+/* 26.06.08 add 정다희 : bi_box 추가*/
+.body_wrap .cont_area .cont_inner .bi_box { padding: 56px 64px; background: #fff; border-radius: 12px; }
+.bi_box > .bi_section{width: 100%;}
+.bi_box > .bi_section > h4 { margin-bottom: 40px; font-size: 3.2rem; font-weight: 700; }
+.bi_box > .bi_section > span > img { width: auto; height: 100%; margin: 0 auto; display: block; object-fit: cover; }
 @media screen and (max-width: 1160px) {
     .body_wrap .cont_area .cont_inner ul {flex-direction:column; align-items:flex-start; gap:16px}
 }
 @media screen and (max-width: 1024px) {}
 @media screen and (max-width: 768px) {
     .visual_img{height:100vh;}
-    .visual_img img{height:100%; object-fit:cover; object-position:65% center; transform:scale(1.18); transform-origin:center;}
+    .visual_img img{height:100%; object-fit:cover; object-position: 29% center; transform-origin:center;}
     .text_box span {color:#fff; font-size:32px; font-weight:700; text-align:center; display: block;  }
     .body_wrap .cont_area {padding:140px 0px;}
+    .body_wrap .cont_area .cont_inner ul {margin-bottom:40px;}
     .body_wrap .cont_area .cont_inner h3 {margin-bottom:16px; color:#161616; font-size:24px; font-weight:700;}
     .body_wrap .cont_area .cont_inner ul {display:flex; justify-content:space-between; align-items:flex-start;}
     .body_wrap .cont_area .cont_inner ul li p {color:#161616; font-size:16px; font-weight:400; line-height:1.5;}
+    .body_wrap .cont_area .cont_inner ul li a{font-size: 1.4rem; line-height: 1.4;}
+    .body_wrap .cont_area .cont_inner .bi_box { padding: 32px; }
+    .bi_box > .bi_section > h4 { margin-bottom: 24px; font-size: 1.8rem; }
+    .body_wrap .cont_area .cont_inner .bi_box > .bi_section > span > img { width: auto; height: 100%; margin: 0 auto; display: block; object-fit: cover; }
     .btn_list_back span {font-size:16px;}
     .btn_big.border {margin-top:80px; margin-bottom:80px; display:flex; justify-content: center;}
     :deep(.cont_inner) h3 br {display:block;}
