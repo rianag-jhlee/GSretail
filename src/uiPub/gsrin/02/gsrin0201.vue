e54bc55646ff69d934dcf102995d8ad132773931
정다희 <ekgml5233@gmail.com>
2026-06-22 09:23:02 +0900

    Merge branch 'main' of https://github.com/rianag-jhlee/GSretail into dhjeong

--- src/uiPub/gsrin/02/gsrin0201.vue
+++ src/uiPub/gsrin/02/gsrin0201.vue
@@ -9,7 +9,7 @@
             <div class="cont_inner">
                 <Tabs v-model="CTabIdx" :tab-items="t.Tabs1" tab-class="type_01" :tab-slide="true" @change="onTabChange1" />
                 
-                
+                <!--  -->
                 <div class="tab_content_wrap">
                     <section class="tab_content gsrin0201" v-if="CTabIdx === 0" :aria-label="t.Tabs1[0].item">
                         <div class="performance_charts">
