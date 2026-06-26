129b588a4482c4428095fd28bd17307bc9c1a334
정다희 <ekgml5233@gmail.com>
2026-06-26 13:09:41 +0900

    feat: 텍스트 수정, css수정

--- src/uiPub/popUp/gsrbr0404.vue
+++ src/uiPub/popUp/gsrbr0404.vue
@@ -17,10 +17,11 @@
                         <em>{{ t.icontxt[idx] }}</em>
                     </li>
                 </ul>
-                
-                <div class="notice_bar">
+                <!-- 26.06.26 del 정다희 : 삭제-->
+                <!-- <div class="notice_bar">
                     <p>{{ t.app_desc }}</p>
-                </div>
+                </div> -->
+                <!-- //26.06.26 del 정다희 : 삭제-->
                 <!-- <i>{{t.app_desc}}</i> -->
             </section>
             <section class="gsfresh_event">
@@ -48,7 +49,7 @@ export default {
                     MainTitle: "행사/이벤트",
                     sub_Title1: "GS25",
                     sub_desc1:"'우리동네GS'에서만 만나볼 수 있는 파격 혜택!",
-                    sub_detail_1:`<em>1+1, 2+1, 덤증정, POP할인 상품들을 우리동네GS앱에서도 구매하실 수 있습니다.</em><em>온라인앱 전용 행사 상품들도 빨빠르게 확인하세요!</em>`,
+                    sub_detail_1:`<em>1+1, 2+1, 덤증정, POP할인 상품들을 우리동네GS앱에서도 구매하실 수 있습니다.<br />우리동네GS 앱에서 <span>GS25 이달의 행사</span>와 온라인앱 전용 행사 상품들도 빨 빠르게 확인하세요!</em>`, /*26.06.26 edit 정다희 : 텍스트 수정*/ 
                     sub1 : {
                         img: require("@/assets/images/dummy/gsrbr0404_1.png"),
                         imgMo: require("@/assets/images/dummy/gsrbr0404_1_mo.png"),
@@ -68,15 +69,15 @@ export default {
                     icontxt: [`1+1`, `2+1`, `덤증정`, `가격할인`],
                     sub_title2: "GS THE FRESH",
                     sub_desc2:"매주 달라지는 행사 미리보기",
-                    sub_detail_2:`<em>단골 매장에서 진행하는 주간 행사와 추천 상품을 확인할 수 있습니다.</em>`,
-                    app_desc:`온라인앱 전용 행사 상품들도 발 빠르게 확인하세요!`
+                    sub_detail_2:`<em>우리동네GS 앱에서 단골 매장의 <span>주간 행사 상품과 행사전단</span>을 편리하게 확인할 수 있습니다.</em>`, /*26.06.26 edit 정다희 : 텍스트 수정*/
+                    // app_desc:`온라인앱 전용 행사 상품들도 발 빠르게 확인하세요!` /*26.06.26 del 정다희 : 삭제*/
                 },
                 en: {
                     closeLabel: "Close",
                     MainTitle: "Events & Promotions",
                     sub_Title1: "GS25",
                     sub_desc1:"Exclusive Deals Available Only on Our Neighborhood GS!",
-                    sub_detail_1:`Buy-One-Get-One, 2+1, bonus gifts, and POP discount items are now available on the Our Neighborhood GS app. Check out app-exclusive promotions before they sell out!`,
+                    sub_detail_1:`ou can also purchase 1+1, 2+1, bonus gift, and POP discount items on the Woori Dongne GS app.<br />Check out <span>GS25 Monthly Promotions</span> and online app-exclusive sale items quickly on the Woori Dongne GS app!`,
                     sub1 : {
                         img: require("@/assets/images/dummy/gsrbr0404_1.png"),
                         imgMo: require("@/assets/images/dummy/gsrbr0404_1_mo.png"),
@@ -96,8 +97,8 @@ export default {
                     icontxt: [`1+1`, `2+1`, `Bonus Gift`, `Price Discount`],
                     sub_title2: "GS THE FRESH",
                     sub_desc2:"Weekly Promotions Preview",
-                    sub_detail_2:`Check out the weekly promotions and recommended products at your regular store.`,
-                    app_desc:`Stay up to date on online app-exclusive promotional products, too!`/* 260604 번역 */
+                    sub_detail_2:`<em>You can conveniently check your regular store's <span>weekly promotional items and promotional flyers</span> on the Woori Dongne GS app.</em>` /*26.06.26 edit 정다희 : 텍스트 수정*/ 
+                    // app_desc:`Stay up to date on online app-exclusive promotional products, too!`/* 260604 번역 */ /*26.06.26 del 정다희 : 삭제*/
                 }
             }
         };
@@ -124,9 +125,11 @@ h3 { margin-bottom: 60px; color: #161616; font-size: 3.2rem; font-weight: 700; }
 h4 {margin-top:24px; color:#161616; font-size:3.2rem; font-weight:700;}
 span {margin:8px 0; color:#161616; font-size:1.8rem; font-weight:700; display:block;}
 span img {max-width:100%; object-fit:cover; border-radius:12px; }
-section > p {display:flex; flex-direction:column; gap:4px;}
+.gs25_event h4 + span{margin: 16px 0 8px;}
+section > p {display:flex; flex-direction:column; gap:4px;font-size: 1.6rem;line-height: 1.5;letter-spacing: -0.01em;}
 :deep(.gs25_event p em), :deep(.gsfresh_event p em) { color:#67676F; font-size:1.6rem;}
-.event_icon_list { display: flex; flex-wrap: wrap; gap: 8px; width: 100%; margin-top: 40px; margin-bottom: 24px; list-style: none; }
+:deep(.gs25_event p em span), :deep(.gsfresh_event p em span) { font-weight:700; }
+.event_icon_list { display: flex; flex-wrap: wrap; gap: 8px; width: 100%; margin-top: 40px; }
 .event_icon_list li { display: flex; flex-direction: column; align-items: center; justify-content: space-between; width: calc((100% - (8px * 3)) / 4); }
 .event_icon_list li .img_box { display: flex; align-items: center; justify-content: center; width: 100%; height: 142px; padding: 20px; background: #F8F8F8; border-radius: 12px; text-align: center; }
 .event_icon_list li .img_box img { max-width: 64px; max-height: 64px; object-fit: contain; }
